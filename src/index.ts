9c2b4db939ac976c9d0655a7a6a70a0efa24fd24
Matej Jellus <juffalow@juffalow.com>
2022-10-21 13:27:44 +0100

    Add page icon

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'public/', 'index.html'))
 });
 
+app.get('/emoticon_small.png', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'public/', 'emoticon_small.png'))
+});
+
 app.get('/ping', (req, res) => {
   res.json({
     name,
