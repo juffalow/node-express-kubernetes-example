6cb70bd9b0a12e55fbc8c65bd8f31cd9e8da6b63
Matej Jellus <juffalow@juffalow.com>
2022-10-21 13:33:45 +0100

    Load icon from external source

--- src/index.ts
+++ src/index.ts
@@ -20,10 +20,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'public/', 'index.html'))
 });
 
-app.get('/emoticon_small.png', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'public/', 'emoticon_small.png'))
-});
-
 app.get('/ping', (req, res) => {
   res.json({
     name,
